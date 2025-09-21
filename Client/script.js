18097736f1f251db215213dcc1e6530ea9369464
devmilon <dev.milon923@gmail.com>
2025-09-21 15:32:11 +0600

    update server local to AWS

--- Client/script.js
+++ Client/script.js
@@ -18,7 +18,7 @@ function getFirstWord(word) {
   return word.toString().split("")[0];
 }
 let currentWord = "a";
-const socket = io("https://word-bound-parsonal.sarv.live");
+const socket = io("http://13.202.211.217:3000");
 const userId = Math.random().toString()?.split(".")[1];
 const joinbtn = document.getElementById("joinbtn");
 const myname = document.getElementById("myname");
